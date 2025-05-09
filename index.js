85ac21aa917712e087c1cfd9c3876ed05ac0f71f
Blessing Lartey <bbklartey@gmail.com>
2025-05-09 14:49:52 +0000

    update

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 await mongoose.connect(process.env.MONGO_URL)
 
 // Start the server
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
