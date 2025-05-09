95fc4a4ca5678e9e2b1d87cd90a44710bca35f65
Blessing Lartey <bbklartey@gmail.com>
2025-05-09 16:31:02 +0000

    update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {registerUser, loginUser, updateUser, getAuthenticatedUser } from "../controllers/userController.js"
+import {registerUser, loginUser, updateUser } from "../controllers/userController.js"
 import { isAuthenticated } from "../middlewares/auth.js";
 
 // create user router
@@ -9,7 +9,7 @@ const userRouter = Router();
 userRouter.post('/users/register', registerUser);
 userRouter.post('/users/login', loginUser);
 userRouter.patch('/users/:id', updateUser);
-userRouter.get('/users/me', isAuthenticated, getAuthenticatedUser)
+userRouter.get('/users/me', isAuthenticated)
 
 
 //Export router
